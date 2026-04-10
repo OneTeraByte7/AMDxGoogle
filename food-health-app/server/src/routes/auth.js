d15584f6cd6c845e1ed55426943fdc07eef77169
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-04-10 20:10:52 +0530

    routing:routes

--- food-health-app/server/src/routes/auth.js
+++ food-health-app/server/src/routes/auth.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/auth');
-const { upsertProfile, getProfile } = require('../controllers/authController');
+const { upsertProfile, getProfile, getSummary } = require('../controllers/authController');
 
 router.post('/profile', verifyToken, upsertProfile);
 router.get('/profile', verifyToken, getProfile);
+router.get('/summary', verifyToken, getSummary);
 
 module.exports = router;
